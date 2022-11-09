40a1fe808a23a028a6ec4543d13f39e66fc8d315
ixidion <ixidion.ninja@gmail.com>
2022-11-09 08:08:28 +0100

    Scripts fixed

--- ressources/build.cmd
+++ ressources/build.cmd
@@ -1,6 +1,7 @@
 @echo off
 cd ..
+cargo clean -r
 cargo build -r
-copy target\release\fintools.exe C:\Users\plabo\portable\fintools
-copy ressources\fintools_starter.ps1 C:\Users\plabo\portable\fintools
+copy %cd%\target\release\fintools.exe C:\Users\plabo\portable\fintools
+copy %cd%\ressources\fintools_starter.ps1 C:\Users\plabo\portable\fintools
 pause
--- ressources/fintools_starter.ps1
+++ ressources/fintools_starter.ps1
@@ -1,4 +1,4 @@
-$env:FINAPP__CACHE_FILE = 'N:\\weekly_symbols\\cache\\symbol_cache.json'
-$env:FINAPP__OUPUT_PATH = 'N:\\weekly_symbols\\new'
-$env:FINAPP__OUPUT_PATH_DIFF = 'N:\\weekly_symbols\\diffs'
+Set-Variable $env:FINAPP__CACHE_FILE = 'N:\\weekly_symbols\\cache\\symbol_cache.json'
+Set-Variable $env:FINAPP__OUTPUT_PATH = 'N:\\weekly_symbols\\new'
+Set-Variable $env:FINAPP__OUTPUT_PATH_DIFF = 'N:\\weekly_symbols\\diffs'
 Start-Process fintools.exe
