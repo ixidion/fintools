c7541ba72f34d20a811943e4e9c8d40557a9a820
ixidion <ixidion.ninja@gmail.com>
2022-11-11 19:38:27 +0100

    Path-Bug fixed

--- src/finlibs/utils.rs
+++ src/finlibs/utils.rs
@@ -1,7 +1,22 @@
 use chrono::Local;
 use std::path::{Path, PathBuf};
 
+use std::env;
+use std::io;
 
+use path_clean::PathClean;
+
+pub fn absolute_path(path: impl AsRef<Path>) -> io::Result<PathBuf> {
+    let path = path.as_ref();
+
+    let absolute_path = if path.is_absolute() {
+        path.to_path_buf()
+    } else {
+        env::current_dir()?.join(path)
+    }.clean();
+
+    Ok(absolute_path)
+}
 
 pub fn formatted_timestamp() -> String {
     let now = Local::now();
